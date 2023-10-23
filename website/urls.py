ace3bbefceb03401777033a4c5aadf4152c1cc79
Abdullahi Ahmed <ilearntostudy@gmail.com>
2023-10-23 14:15:52 +0300

    registration

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for website app in dcrm project.
+"""
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+""" views module of website app"""
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home(request):
+    """ home page view"""
+    # check if the user is logged in
+    if request.method == 'POST':
+        # do something
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have logged int")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logged in")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    """"Logout function"""
+    logout(request)
+    messages.success(request, "You have logged out")
+    return redirect('home')
+
+
+def register_user(request):
+    """registering user function"""
+    return render(request, 'register.html', {})
