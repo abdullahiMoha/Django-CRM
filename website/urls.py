978c69d8a09944a572d2dd0506335e7ae15f7333
Abdullahi Ahmed <ilearntostudy@gmail.com>
2023-10-27 10:37:06 +0300

    Final Project

--- website/urls.py
+++ website/urls.py
@@ -2,11 +2,15 @@
 URL configuration for website app in dcrm project.
 """
 from django.urls import path
-from . import views
+from .import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.user_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from website.models import Record
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 # Create your views here.
 
 
@@ -55,3 +55,60 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+
+def user_record(request, pk):
+    """view users record method"""
+    if request.user.is_authenticated:
+        # look up the user record
+        # pylint: disable=E1101
+        user_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'user_record': user_record})
+    else:
+        messages.success(request, "You Must Login to View User")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    """delete record method"""
+    if request.user.is_authenticated:
+        # pylint: disable=E1101
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "You Have deleted User Suceessfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must be Logged in to delete a User")
+        return redirect('home')
+
+
+def add_record(request):
+    """adding new record function"""
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "New Record Added Successfully...")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You must e logged in  to add a record...")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    """Updating record view function"""
+    if request.user.is_authenticated:
+        # pylint: disable=E1101
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, "Record has been Updated Successfully...")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You must e logged in  to add a record...")
+        return redirect('home')
